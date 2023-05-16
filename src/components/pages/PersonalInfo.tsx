a2735ab3c75b5c0a57cb0af70b486426a8495691
Tomas Scerbak <tomas.scerbak@gmail.com>
2023-05-16 22:56:51 +0200

    added basic form into personalinfo component

--- src/components/pages/PersonalInfo.tsx
+++ src/components/pages/PersonalInfo.tsx
@@ -1,5 +1,28 @@
+type Props = {
+  header: string;
+  description: string;
+};
+
 const PersonalInfo = () => {
-  return <div>PersonalInfo</div>;
+  return (
+    <div>
+      <h2>Please provide your name, email address, and phone number.</h2>
+      <form>
+        <div>
+          <label htmlFor="name">Name</label>
+          <input type="text" name="name" />
+        </div>
+        <div>
+          <label htmlFor="email">Email Address</label>
+          <input type="email" name="email" />
+        </div>
+        <div>
+          <label htmlFor="phone">Phone Number</label>
+          <input type="tel" name="phone" />
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default PersonalInfo;
