10eed64786242326627901cf50216d8a55451b1b
Tomas Scerbak <tomas.scerbak@gmail.com>
2023-08-12 21:35:34 +0200

    finalized styles for mobile version

--- src/components/pages/ThankYou.tsx
+++ src/components/pages/ThankYou.tsx
@@ -3,6 +3,8 @@ import { RootState } from "../../store";
 
 import ThankYouIcon from "../../assets/icon-thank-you.svg";
 
+import classes from "./ThankYou.module.css";
+
 const ThankYou = () => {
   const userPlan = useSelector(
     (state: RootState) => state.persistedReducer.userPlan
@@ -14,21 +16,27 @@ const ThankYou = () => {
 
   return (
     <section>
-      {userPlan.type === "" ? (
-        <p>Please choose your plan</p>
-      ) : (
-        <>
-          <img src={ThankYouIcon} alt="thank you" />
-          <header>
-            <p>Thank you {userName}!</p>
-          </header>
-          <p>
-            Thanks for confirming your subscription! We hope you have fun using
-            our platform. If you ever need support, please feel free to email us
-            at support@loremgaming.com.
-          </p>
-        </>
-      )}
+      <div className={classes["thanks-wrapper"]}>
+        {userPlan.type === "" ? (
+          <p>Please choose your plan</p>
+        ) : (
+          <>
+            <img
+              className={classes.thanks}
+              src={ThankYouIcon}
+              alt="thank you"
+            />
+            <header>
+              <p className={classes["thanks__header"]}>Thank you {userName}!</p>
+            </header>
+            <p className={classes["thanks__text"]}>
+              Thanks for confirming your subscription! We hope you have fun
+              using our platform. If you ever need support, please feel free to
+              email us at support@loremgaming.com.
+            </p>
+          </>
+        )}
+      </div>
     </section>
   );
 };
